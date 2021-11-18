0ba972f2324865d5b90e9e43295c3a00dc41f253
Scott Adams <sadams206@caledonian.ac.uk>
2021-11-18 15:37:37 +0000

    person class first commit

---
+++ person.java
@@ -0,0 +1,21 @@
+public class Person
+{
+	String firstName;
+	String otherName;
+	String lastName;
+
+	public Person()
+	{
+		firstName = "";
+		otherName = "";
+		lastname = "";
+	}
+	public person(String value1, String value2)
+	{
+		firstName = value1;
+		lastName = value2;
+		otherName = "";
+	}
+
+
+
